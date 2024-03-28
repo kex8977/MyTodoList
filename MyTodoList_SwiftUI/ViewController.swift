97aba1c7e5079924d28700f9862eb17c57fb4ce1
kex8977 <46492883+kex8977@users.noreply.github.com>
2024-03-29 08:35:31 +0900

    Add files via upload

--- MyTodoList_SwiftUI/ViewController.swift
+++ MyTodoList_SwiftUI/ViewController.swift
@@ -5,6 +5,7 @@
 //
 
 import UIKit
+import Lottie
 
 struct Todo {
    var title : String
@@ -15,19 +16,16 @@ struct Todo {
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
       
    @IBOutlet weak var tableViewEx: UITableView!
-
+   
    var data: [Todo] = [Todo(title: "one", isComplete: false, date: Date.now),
                      Todo(title: "two", isComplete: false, date: Date.now),
                      Todo(title: "three", isComplete: false, date: Date.now)]
-   
-//   var data: [Todo] = [Todo(title: "one", date: Date.now),
-//                     Todo(title: "two", date: Date.now),
-//                     Todo(title: "three", date: Date.now)]
-   
    // 구조체형태의 배열을 선언해줌으로써, 유지보수성, 캡슐화 구현?!
-   
    //var data = ["one", "two", "three"]
    
+  // var animation : LottieAnimation?
+   //var animationView = LottieAnimationView(animation: animation)
+
     override func viewDidLoad() {
         super.viewDidLoad()
        
@@ -36,7 +34,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
        tableViewEx.delegate = self
           
        tableViewEx.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-       //view.addSubview(tableViewEx)
+       
+       let animation = LottieAnimation.named("movingCar")
+       let animationView = LottieAnimationView(animation: animation)
+       
+       animationView.frame = CGRect(x: 16, y: 600, width: 361, height: 230)
+       animationView.loopMode = .loop // 애니메이션 무한 재생
+       view.addSubview(animationView)
+
+       animationView.play()
     }
       
       /* [필수] 테이블의 행 수를 보고합니다. */
@@ -65,8 +71,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
        //액세서리 뷰에 switchControl 달아주기
        cell.accessoryView = switchControl
        
-       print(switchControl.isOn, switchControl.tag, indexPath.row)
-       
        if switchControl.isOn == true {
           cell.textLabel?.attributedText = data[indexPath.row].title.strikeThrough()
        }
@@ -77,13 +81,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
       // 그런데 switchControl.isOn을 제어가 불가능하다 !! 왜??
        // 데이터가 변경되고 난 후 reload 되지 않았기 때문임.
        
-       //스위치의 값이 변경되었을 때, tableViewEx reload 실행
+       //스위치의 값이 변경되었을 때, didTap() 실행
        switchControl.addTarget(self, action: #selector(didTap(_:)), for: .valueChanged)
        
        return cell
     }
    
-   //object C 의 문법?!
+   //switch가 선택되었으므로 tableView 내의 데이터가 변화가 생김, 따라서 reloadData() 메소드 실행
    @objc
    func didTap(_ sender: UISwitch) {
       
@@ -98,8 +102,37 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
        // 선택된 셀 애니메이션 실행
        tableViewEx.deselectRow(at: indexPath, animated: true)
     }
+   
+   // Asks the delegate for the editing style of a row at a particular loacation in a table view
+   // 테이블 뷰 특정 부분의 row 편집 스타일을 delegate에게 요청한다.
+   // 삼항 연산자의 결과를 표현하는 부분에서 나오는 프로퍼티는 enum으로 구성되어 있다. .delete .insert .none
+   // indexPath.row 의 값이 0부터 시작하므로 컴파일 완료시 .delete로 설정됨
+   func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+      return indexPath.row >= 0 ? .delete : .none
+   }
+   
+   //
+   func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+      guard editingStyle == .delete else { return }
       
-   @IBAction func btnNewCreate(_ sender: UIButton) {
+      // 데이터 배열에서 실제 값 삭제
+      data.remove(at: indexPath.row)
+      
+      // 테이블 뷰에서 행 삭제
+      tableViewEx.deleteRows(at: [indexPath], with: .fade)
+   }
+   
+   // indexPath.row 값이 0 이상인 셀만 이동이 가능하도록 설정
+   func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+      return indexPath.row >= 0
+   }
+   
+   // 이동가능한 셀에 삼단바를 표시
+   func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+
+   }
+   
+   @IBAction func btnCreate(_ sender: UIButton) {
       
       //알림창 인스턴스 생성
       let controller = UIAlertController(title:nil, message: "Write Text", preferredStyle: UIAlertController.Style.alert)
@@ -112,11 +145,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
          if let titleTextField = controller.textFields?.first {
             self.data.append(Todo(title: titleTextField.text!, isComplete: false, date: Date.now))
          }
-
-//      let confirm = UIAlertAction(title: "확인", style: .default) { action in
-//         if let titleTextField = controller.textFields?.first {
-//            self.data.append(Todo(title: titleTextField.text!, isComplete: true, date: Date.now))
-//         }
       
          self.tableViewEx.reloadData()
          // data 배열에 항목이 업데이트 되었으니 tableView를 Reload 동작
@@ -127,6 +155,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
       controller.addAction(confirm)
       controller.addAction(close)
       present(controller, animated: true, completion: nil)
+   }
+   
+   @IBAction func btnEdit(_ sender: UIButton) {
+      
+      sender.isSelected.toggle()  // bool 값을 껐다 켰다를 반복
+      
+      if sender.isSelected {
+         sender.setTitle("Done", for: .normal)
+         tableViewEx.isEditing = true // tableView를 편집모드로 진입
+         tableViewEx.delegate = self  // 셀 편집 기능 활성화
+         
+//         animation = LottieAnimation.named("movingCar")
+      }
+      else{
+         sender.setTitle("Edit", for: .normal)
+         tableViewEx.isEditing = false
+         
+//         animation = LottieAnimation.named("scan_IDcard")
+      }
    }
 }
 
@@ -142,5 +189,13 @@ extension String {
       let attributeString = NSMutableAttributedString(string: self)
       attributeString.removeAttribute(NSMutableAttributedString.Key.strikethroughStyle, range: NSMakeRange(0, attributeString.length))
       return attributeString
+   }
+}
+
+extension ViewController {
+   
+   // 아래 메소드가 실행되는 시점은 언제이지? Delete 버튼이 눌렸을 때?
+   func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+      indexPath.row >= 0
    }
 }
